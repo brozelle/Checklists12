8e60d2277b8386d09d85b4cd0097ac823190f34d
Buck <buck@buckrozelle.com>
2020-12-08 17:52:33 -0500

    Chapter 10: Data Model

---
+++ Checklists12/ChecklistItem.swift
@@ -0,0 +1,13 @@
+//
+//  ChecklistItem.swift
+//  Checklists12
+//
+//  Created by Buck Rozelle on 12/8/20.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+}
