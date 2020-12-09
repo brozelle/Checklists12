6b1c46504f116570a479e7398d407c3f11c3d186
Buck <buck@buckrozelle.com>
2020-12-09 17:45:34 -0500

    Chapter 12: Add Item Screen

--- Checklists12/AddItemViewController.swift
+++ Checklists12/AddItemViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,13 +16,53 @@ class AddItemViewController: UITableViewController {
         
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
+    
+    // Mark:- Outlets
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    
+    
     // Mark:- Actions
     @IBAction func cancel() {
         navigationController?.popViewController(animated: true)
     }
     
     @IBAction func done() {
+        print("Contents of the text field: \(textField.text!)")
+        
         navigationController?.popViewController(animated: true)
     }
-
+    
+    // Mark:- Table View Delegates
+    override func tableView(_ tableView: UITableView,
+                            willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    
+    }
+    
+    // MARK: - Text Field Delegates
+    // Disallowing empty input
+    func textField(_ textField: UITextField,
+                   shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+      
+        let oldText = textField.text!
+        let stringRange = Range(range,
+                                in: oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange,
+                                                  with: string)
+        
+        doneBarButton.isEnabled = !newText.isEmpty
+        
+        return true
+    }
+    //handling the clear button
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        doneBarButton.isEnabled = false
+        return true
+    }
 }
